6edbe0346a5f529f04774942e9a3203bbc160679
Richard <ryli721@gmail.com>
2013-02-25 20:41:59 -0500

    volume change works, but not in db scale. Need to unlog

--- src/Main.java
+++ src/Main.java
@@ -1,112 +1,69 @@
+import java.io.File;
 import java.io.IOException;
 import java.lang.Math;
 import com.leapmotion.leap.*;
 import com.leapmotion.leap.Gesture.State;
+import javax.sound.sampled.*;
 
 class SampleListener extends Listener {
-    public void onInit(Controller controller) {
-        System.out.println("Initialized");
-    }
+	public AudioInputStream audioInputStream;
+	public FloatControl gainControl;
 
-    public void onConnect(Controller controller) {
-        System.out.println("Connected");
-    }
+	public void onInit(Controller controller) {
+		System.out.println("Initialized");
+	}
 
-    public void onDisconnect(Controller controller) {
-        System.out.println("Disconnected");
-    }
+	public void onConnect(Controller controller) {
+		System.out.println("Connected");
+		try {
+			audioInputStream = AudioSystem.getAudioInputStream(new File(
+					"th06_05.wav"));
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Clip clip;
+		try {
+			clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			gainControl = (FloatControl) clip
+					.getControl(FloatControl.Type.MASTER_GAIN);
+			System.out.println(gainControl.getMaximum() + "   " + gainControl.getMinimum());
+						
+			clip.start();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-    public void onExit(Controller controller) {
-        System.out.println("Exited");
-    }
+	public void onDisconnect(Controller controller) {
+		System.out.println("Disconnected");
+	}
 
-    public void onFrame(Controller controller) {
-        // Get the most recent frame and report some basic information
-        Frame frame = controller.frame();
-        
-        if (!frame.hands().empty()) {
-            // Get the first hand
-            Hand hand = frame.hands().get(0);
-            hand.palmPosition();
+	public void onExit(Controller controller) {
+		System.out.println("Exited");
+	}
 
+	public void onFrame(Controller controller) {
+		// Get the most recent frame and report some basic information
 
-            // Get the hand's sphere radius and palm position
-            //System.out.println("Hand sphere radius: " + hand.sphereRadius()
-            //                 + " mm, palm position: " + hand.palmPosition());
-
-            // Get the hand's normal vector and direction
-            Vector normal = hand.palmNormal();
-            Vector direction = hand.direction();
-
-            // Calculate the hand's pitch, roll, and yaw angles
-            //System.out.println("Hand pitch: " + Math.toDegrees(direction.pitch()) + " degrees, "
-            //                 + "roll: " + Math.toDegrees(normal.roll()) + " degrees, "
-            //                 + "yaw: " + Math.toDegrees(direction.yaw()) + " degrees");
-        }
-
-        GestureList gestures = frame.gestures();
-        for (int i = 0; i < gestures.count(); i++) {
-            Gesture gesture = gestures.get(i);
-
-            switch (gesture.type()) {
-                case TYPE_CIRCLE:
-                    CircleGesture circle = new CircleGesture(gesture);
-
-                    // Calculate clock direction using the angle between circle normal and pointable
-                    String clockwiseness;
-                    if (circle.pointable().direction().angleTo(circle.normal()) <= Math.PI/4) {
-                        // Clockwise if angle is less than 90 degrees
-                        clockwiseness = "clockwise";
-                    } else {
-                        clockwiseness = "counterclockwise";
-                    }
-
-                    // Calculate angle swept since last frame
-                    double sweptAngle = 0;
-                    if (circle.state() != State.STATE_START) {
-                        CircleGesture previousUpdate = new CircleGesture(controller.frame(1).gesture(circle.id()));
-                        sweptAngle = (circle.progress() - previousUpdate.progress()) * 2 * Math.PI;
-                    }
-
-                    System.out.println("Circle id: " + circle.id()
-                               + ", " + circle.state()
-                               + ", progress: " + circle.progress()
-                               + ", radius: " + circle.radius()
-                               + ", angle: " + Math.toDegrees(sweptAngle)
-                               + ", " + clockwiseness);
-                    break;
-                case TYPE_SWIPE:
-                    SwipeGesture swipe = new SwipeGesture(gesture);
-                    System.out.println("Swipe id: " + swipe.id()
-                               + ", " + swipe.state()
-                               + ", position: " + swipe.position()
-                               + ", direction: " + swipe.direction()
-                               + ", speed: " + swipe.speed());
-                    break;
-                case TYPE_SCREEN_TAP:
-                    ScreenTapGesture screenTap = new ScreenTapGesture(gesture);
-                    System.out.println("Screen Tap id: " + screenTap.id()
-                               + ", " + screenTap.state()
-                               + ", position: " + screenTap.position()
-                               + ", direction: " + screenTap.direction());
-                    break;
-                case TYPE_KEY_TAP:
-                    KeyTapGesture keyTap = new KeyTapGesture(gesture);
-                    System.out.println("Key Tap id: " + keyTap.id()
-                               + ", " + keyTap.state()
-                               + ", position: " + keyTap.position()
-                               + ", direction: " + keyTap.direction());
-                    break;
-                default:
-                    System.out.println("Unknown gesture type.");
-                    break;
-            }
-        }
-
-        if (!frame.hands().empty() || !gestures.empty()) {
-            //System.out.println();
-        }
-    }
+		Frame frame = controller.frame();
+		float h;
+		if (!frame.hands().empty()) {
+			// Get the first hand
+			Hand hand = frame.hands().get(0);
+			System.out.println(hand.palmPosition().getY());
+			h=(hand.palmPosition().getY()-30)/500;
+			h=(float) Math.min(Math.max(h, 0),1.0);
+			h=(1-h)*(gainControl.getMinimum());
+			
+			gainControl.setValue(h);
+		}
+	}
 }
 
 public class Main {
